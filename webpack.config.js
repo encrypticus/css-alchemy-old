893017116650a64f7c146696756f83173a9695b8
encrypticus <encrypticus@gmail.com>
2020-03-24 21:17:06 +0600

    Активировал copy webpack plugin в конфиге сборщика.

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ const enableStylelint = require('./webpack/plugins/stylelint-webpack-plugin'); /
 const processJs = require('./webpack/presets/js'); // пресет обрабатывает js-файлы
 const browserSync = require('./webpack/plugins/browser-sync-webpack-plugin'); // плагин browser sync
 const cleanWebpackPlugin = require('./webpack/plugins/clean-webpack-plugin'); // плагин очищает папку сборки перед каждой пересборкой
+const copyPlugin = require('./webpack/plugins/copy-webpack-plugin'); // плагин копирует файлы согласно переданным при вызове настройкам
 
 module.exports = () => {
   const commonConfig = webpackMerge(
@@ -35,7 +36,10 @@ module.exports = () => {
     processPug(),
     processJs(),
     cleanWebpackPlugin(),
-    // enableStylelint()
+    enableStylelint(),
+    copyPlugin([
+      { from: 'src/favicons', to: 'favicons/' }
+    ])
   );
 
   if (process.env.mode === 'development') {
