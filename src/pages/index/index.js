df8d39e01e1f58e05fbe2491cfc31f7b6a0053cf
encrypticus <encrypticus@gmail.com>
2020-03-26 22:52:29 +0600

    Подключил компонент hamburger.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,3 +4,4 @@ import '../../sass/common.scss';
 import '../../blocks/page-header/page-header';
 import '../../components/socials/socials';
 import '../../components/site-logo/site-logo';
+import '../../components/hamburger/hamburger';
